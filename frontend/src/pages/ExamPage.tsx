b6af8b17d6ee9d23c8cf78a44c75fd1b76aa1f6d
Ankit <ankit.saini@ksolves.com>
2025-03-20 16:08:32 +0530

    Fixes to display exam only after proctoring tool has started.

--- frontend/src/pages/ExamPage.tsx
+++ frontend/src/pages/ExamPage.tsx
@@ -10,7 +10,7 @@ import { useAuth } from "../authContext/AuthContext";
 const ExamPage: React.FC = () => {
   const { studentExamId } = useParams();
   const { user } = useAuth();
-  
+
   const navigate = useNavigate();
 
   const [exam, setExam] = useState<Exam | null>(null);
@@ -19,6 +19,7 @@ const ExamPage: React.FC = () => {
     [key: number]: number | number[] | boolean | null;
   }>({});
   const [smowlUrl, setSmowlUrl] = useState("");
+  const [proctoringReady, setProctoringReady] = useState(false);
 
   useEffect(() => {
     const fetchMonitoringUrl = async () => {
@@ -35,6 +36,25 @@ const ExamPage: React.FC = () => {
   }, [user, studentExamId]);
 
   useEffect(() => {
+    const handleMessage = (event: MessageEvent) => {
+      if (event.origin !== "https://app.smowltech.net") return;
+
+      if (event.data === "monitoringstatusOK") {
+        setProctoringReady(true);
+        toast.success("Exam started successfully!", { autoClose: 3000 });
+      } else if (event.data === "monitoringstatusNOTOK") {
+        setProctoringReady(false);
+        toast.error(
+          "⚠️ SMOWL CM is closed! Restart SMOWL CM to continue the exam."
+        );
+      }
+    };
+
+    window.addEventListener("message", handleMessage);
+    return () => window.removeEventListener("message", handleMessage);
+  }, []);
+
+  useEffect(() => {
     const fetchExamDetails = async () => {
       try {
         const response = await getStudentExamById(Number(studentExamId));
@@ -44,15 +64,16 @@ const ExamPage: React.FC = () => {
         const initialResponses: {
           [key: number]: number | number[] | boolean | null;
         } = {};
-        response.data.studentExamDetails.exam.questions.forEach((q: Question) => {
-          if (q.question_type === "true_false") {
-            initialResponses[q.question_id!] = null;
-          } else {
-            initialResponses[q.question_id!] = [];
+        response.data.studentExamDetails.exam.questions.forEach(
+          (q: Question) => {
+            if (q.question_type === "true_false") {
+              initialResponses[q.question_id!] = null;
+            } else {
+              initialResponses[q.question_id!] = [];
+            }
           }
-        });
+        );
         setResponses(initialResponses);
-        toast.success("Exam started successfully!", { autoClose: 3000 });
       } catch (error: any) {
         toast.error(error.response?.data?.message);
       } finally {
@@ -140,93 +161,108 @@ const ExamPage: React.FC = () => {
   if (!exam) return <p>No exam found!</p>;
 
   return smowlUrl ? (
-    <div className="d-flex mt-3">
-    <Container>
-      <h2>{exam.title}</h2>
-      <p>Duration: {exam.duration} minutes</p>
-      {exam.questions.map((question: Question, index: number) => (
-        <div key={question.question_id} className="mb-4">
-          <h4>
-            {index + 1}. {question.question_text}
-          </h4>
-          <Form>
-            {question.question_type !== "true_false" ? (
-              question.options?.map((option: Option) => (
-                <Form.Check
-                  key={option.option_id}
-                  type={
-                    question.question_type === "multi_select"
-                      ? "checkbox"
-                      : "radio"
-                  }
-                  label={option.option_text}
-                  name={`question_${question.question_id}`}
-                  checked={
-                    question.question_type === "multi_select"
-                      ? (
-                          responses[question.question_id!] as number[]
-                        )?.includes(option.option_id!) || false
-                      : responses[question.question_id!] === option.option_id
-                  }
-                  onChange={() =>
-                    handleOptionChange(
-                      question.question_id!,
-                      option.option_id!,
-                      question.question_type
-                    )
-                  }
-                />
-              ))
-            ) : (
-              <>
-                <Form.Check
-                  type="radio"
-                  label="True"
-                  name={`question_${question.question_id}`}
-                  checked={responses[question.question_id!] === true || false}
-                  onChange={() =>
-                    handleOptionChange(
-                      question.question_id!,
-                      true,
-                      "true_false"
-                    )
-                  }
-                />
-                <Form.Check
-                  type="radio"
-                  label="False"
-                  name={`question_${question.question_id}`}
-                  checked={responses[question.question_id!] === false || false}
-                  onChange={() =>
-                    handleOptionChange(
-                      question.question_id!,
-                      false,
-                      "true_false"
-                    )
-                  }
-                />
-              </>
-            )}
-          </Form>
+    <>
+      <h5 className="text-muted mt-3">
+        <span className="text-danger">Important:</span> Do not close the "SMOWL CM" application while taking the
+        exam, as doing so may result in your exam restarting.
+      </h5>
+      <div className="d-flex">
+        <Container>
+          <h2>{exam.title}</h2>
+          <p>Duration: {exam.duration} minutes</p>
+          {!proctoringReady ? (
+            <p> **Waiting for SMOWL Proctoring to Start...**</p>
+          ) : (
+            <>
+              {exam.questions.map((question: Question, index: number) => (
+                <div key={question.question_id} className="mb-4">
+                  <h4>
+                    {index + 1}. {question.question_text}
+                  </h4>
+                  <Form>
+                    {question.question_type !== "true_false" ? (
+                      question.options?.map((option: Option) => (
+                        <Form.Check
+                          key={option.option_id}
+                          type={
+                            question.question_type === "multi_select"
+                              ? "checkbox"
+                              : "radio"
+                          }
+                          label={option.option_text}
+                          name={`question_${question.question_id}`}
+                          checked={
+                            question.question_type === "multi_select"
+                              ? (
+                                  responses[question.question_id!] as number[]
+                                )?.includes(option.option_id!) || false
+                              : responses[question.question_id!] ===
+                                option.option_id
+                          }
+                          onChange={() =>
+                            handleOptionChange(
+                              question.question_id!,
+                              option.option_id!,
+                              question.question_type
+                            )
+                          }
+                        />
+                      ))
+                    ) : (
+                      <>
+                        <Form.Check
+                          type="radio"
+                          label="True"
+                          name={`question_${question.question_id}`}
+                          checked={
+                            responses[question.question_id!] === true || false
+                          }
+                          onChange={() =>
+                            handleOptionChange(
+                              question.question_id!,
+                              true,
+                              "true_false"
+                            )
+                          }
+                        />
+                        <Form.Check
+                          type="radio"
+                          label="False"
+                          name={`question_${question.question_id}`}
+                          checked={
+                            responses[question.question_id!] === false || false
+                          }
+                          onChange={() =>
+                            handleOptionChange(
+                              question.question_id!,
+                              false,
+                              "true_false"
+                            )
+                          }
+                        />
+                      </>
+                    )}
+                  </Form>
+                </div>
+              ))}
+              <Button variant="success" onClick={handleSubmit}>
+                Submit Exam
+              </Button>
+            </>
+          )}
+        </Container>
+        <div className="mt-2">
+          <iframe
+            src={smowlUrl}
+            width="100%"
+            height="400px"
+            frameBorder="0"
+            allow="microphone; camera"
+            sandbox="allow-same-origin allow-scripts allow-popups"
+          />
         </div>
-      ))}
-
-      <Button variant="success" onClick={handleSubmit}>
-        Submit Exam
-      </Button>
-    </Container>
-  
-    <div>
-        <iframe
-          src={smowlUrl}
-          width="100%"
-          height="400px"
-          frameBorder="0"
-          allow="microphone; camera"
-          sandbox="allow-same-origin allow-scripts allow-popups"
-        />
       </div>
-    </div>
+    </>
   ) : (
     <p>Loading proctoring session...</p>
   );
